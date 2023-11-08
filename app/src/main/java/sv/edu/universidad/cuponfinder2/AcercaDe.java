3ea6207aaca95519770c50ae164c0cda345e24c8
angcm25 <merari.perdomo@catolica.edu.sv>
2023-11-07 22:31:55 -0600

    boton regresar acerca de

--- app/src/main/java/sv/edu/universidad/cuponfinder2/AcercaDe.java
+++ app/src/main/java/sv/edu/universidad/cuponfinder2/AcercaDe.java
@@ -3,12 +3,17 @@ package sv.edu.universidad.cuponfinder2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AcercaDe extends AppCompatActivity {
 
+    Button btnRegresarAcercaDe;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_acerca_de);
+        btnRegresarAcercaDe=(Button) findViewById(R.id.btnRegresarAcercaDe);
+        btnRegresarAcercaDe.setOnClickListener(v -> onBackPressed());
     }
 }
