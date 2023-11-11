8c91043ef477b924b8eb3096a7c780845f2cf4e2
angcm25 <99634830+MerariPerdomo@users.noreply.github.com>
2023-11-10 23:54:29 -0600

    Cambio de idiomas

--- app/src/main/java/sv/edu/universidad/cuponfinder2/Configuracion.java
+++ app/src/main/java/sv/edu/universidad/cuponfinder2/Configuracion.java
@@ -3,11 +3,13 @@ package sv.edu.universidad.cuponfinder2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
+
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
 public class Configuracion extends AppCompatActivity {
     Button btnRegresarConfig;
+    Button btnCambiarLenguaje;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +18,24 @@ public class Configuracion extends AppCompatActivity {
 
         btnRegresarConfig=(Button) findViewById(R.id.btnRegresarConfig);
         btnRegresarConfig.setOnClickListener(v -> onBackPressed());
+
+        btnCambiarLenguaje = findViewById(R.id.btnCambiarLenguaje);
+        btnCambiarLenguaje.setOnClickListener(v -> showChangeLangDialog());
+    }
+    public void showChangeLangDialog() {
+        String[] listItems = {"English", "Español(Aun no disponible)"};
+
+        MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(this);
+        builder.setTitle("Choose a language");
+        builder.setSingleChoiceItems(listItems, -1, (dialog, which) -> {
+            if (which == 0) {
+                ((MyApplication) getApplication()).updateLocale("en");
+            } else if (which == 1) {
+                ((MyApplication) getApplication()).updateLocale("en");
+            }
+            dialog.dismiss();
+            recreate(); // Reinicia la actividad para aplicar los cambios de idioma
+        });
+        builder.show();
     }
 }
